3b3ccc26a2f8e06e5c06e0470c47a6d887eaff3c
clwaddel <30387984+clwaddel@users.noreply.github.com>
2018-02-24 17:09:23 -0500

    first commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+tar -xvf xmr-stak-2.2.0.tar.gz
+cd xmr-stak-2.2.0/
+cd xmrstak/
+sudo cmake -DCMAKE_INSTALL_PREFIX=/usr/local/bin/miners/xmr-stak-2.2.0/ -DCUDA_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF -DOpenCL_LIBRARY=/usr/lib/x86_64-linux-gnu/libOpenCL.so -DOpenCL_INCLUDE_DIR=/usr/include/  ..
+sudo make install
+cd ..
+cd ..
