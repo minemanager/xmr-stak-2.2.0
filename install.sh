31c23a7d0410b4bb93304dc2fed3e2af54a359ba
clwaddel <30387984+clwaddel@users.noreply.github.com>
2018-02-26 15:59:14 -0500

    test

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt-get install --yes libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
 cd xmrstak/
 sudo cmake -DCMAKE_INSTALL_PREFIX=/usr/local/bin/miners/xmr-stak-2.2.0/ -DCUDA_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF -DOpenCL_LIBRARY=/usr/lib/x86_64-linux-gnu/libOpenCL.so -DOpenCL_INCLUDE_DIR=/usr/include/  ..
 sudo make install
